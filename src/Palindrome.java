851c2897d7f176c5c768caca76e3fa3015a714d2
b5710547221 <warat.n@ku.th>
2017-10-04 17:42:40 +0700

    last update

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,3 +1,4 @@
+
 /**
  * Test if a word or phrase is a palindrome.
  * Should work with any strings.
@@ -5,51 +6,70 @@
  * @author your name
  */
 import java.util.*;
+
 public class Palindrome {
-    static Scanner a = new Scanner(System.in);
+	static Scanner a = new Scanner(System.in);
+
 	/**
-	 * Test if the parameter is a palindrome, 
-	 * ignoring spaces in the string.
-	 * @param phrase is the phrase to test
+	 * Test if the parameter is a palindrome, ignoring spaces in the string.
+	 * 
+	 * @param phrase
+	 *            is the phrase to test
 	 * @return true if it is a palindrom
 	 */
 	public static boolean isPalindrome(String phrase) {
-		boolean isPalin=true;
-		for(int i = 0;i<phrase.length() && phrase.length()%2==0;i++){
-			
-			if(phrase.charAt(i)==(phrase.charAt(phrase.length()-i-1))){
-				isPalin =  true;
-			}else {
-				isPalin = false;
-			}
-		}
-        for(int i = 0;i<phrase.length()/2 && phrase.length()%2!=0;i++){
-			
-			if(phrase.charAt(i)==(phrase.charAt(phrase.length()-i-1))){
+		
+		boolean isPalin = true;
+		for (int i = 0; i < phrase.length() && phrase.length() % 2 == 0; i++) {
+
+			if (phrase.charAt(i) == (phrase.charAt(phrase.length() - i - 1))) {
 				isPalin = true;
-			}else {
+			} else {
 				isPalin = false;
 			}
 		}
-		return isPalin; 	
+		for (int i = 0; i < phrase.length() / 2 && phrase.length() % 2 != 0; i++) {
+
+			if (phrase.charAt(i) == (phrase.charAt(phrase.length() - i - 1))) {
+				isPalin = true;
+			} else {
+				isPalin = false;
+			}
 		}
-	
+		return isPalin;
+	}
+
 	/**
 	 * some simple tests to get you started.
 	 */
 	public static void testPalindrome() {
-		String[] tests = {"dad","Mad","12344321","I","Do geese see god","กา ฝาก","beab"};
-		for(String phrase: tests) {
+		String[] tests = { "dad", "Mad", "12344321", "I", "Do geese see god", "กา ฝาก", "beab" };
+		for (String phrase : tests) {
 			System.out.printf("Phrase: '%s' ", phrase);
 			boolean palindrome = isPalindrome(phrase);
-			System.out.printf("%s a palindrome%n", (palindrome?"is":"is not"));
+			System.out.printf("%s a palindrome%n", (palindrome ? "is" : "is not"));
 		}
 	}
+
 	public static void main(String[] args) {
 		testPalindrome(); // you can commit this out after it works
 		// write code to ask user for a word or phrase
 		// read the WHOLE LINE and test for palindrome
-	    String test  = a.next();
-	    System.out.printf("Is this a palindrome?, %s",(isPalindrome(test))?"yes":"no");
+		System.out.println("Enter a blank line to quit.");
+		String test = " ";
+		String testfinal =" ";
+		String testlast =" ";
+		while (true) {
+			System.out.print("Enter a phrase: ");
+			test = a.nextLine();
+			if(test.equals("")){
+				break;
+			}
+			testfinal = test.toLowerCase();
+			testlast = testfinal.replaceAll(" ", "");
+			
+			System.out.printf(test + " is %s\n", (isPalindrome(testlast)) ? "a palindrome" : "not a palindrome");
+		}
+
 	}
 }
